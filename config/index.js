0f8ebdf1ad77e163d0d780ad96ba934a9d505751
sagar pednekar <sagarpednekar04@gmail.com>
2019-05-14 14:51:31 +0530

    Fixed(Config: Added config file to store global configurations)

---
+++ config/index.js
@@ -0,0 +1,11 @@
+const config = {
+   PORT: process.env.PORT || 4000,
+   pusher: {
+      APP_ID: "780756",
+      KEY: "93d33b6256a37fd6c03e",
+      SECRET: "48fd016514f6a8b44659",
+      CLUSTER: "ap2"
+   }
+};
+
+module.exports = config;
